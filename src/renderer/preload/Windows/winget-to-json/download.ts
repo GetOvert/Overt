613ba5ba898921fe61be42e7dbdf810db17b841a
Ian Gregory <justcheesy@outlook.com>
2022-05-23 16:52:08 -0400

    Fix problems downloading winget packages archive

--- src/renderer/preload/Windows/winget-to-json/download.ts
+++ src/renderer/preload/Windows/winget-to-json/download.ts
@@ -1,16 +1,23 @@
 import fs from "fs";
 import path from "path";
 import os from "os";
-import http from "http";
+import fetch from "node-fetch";
 import StreamZip from "node-stream-zip";
 
 export async function downloadWingetManifests(url: string): Promise<string> {
   return new Promise((resolve, reject) => {
-    fs.mkdtemp(path.join(os.tmpdir(), "winget-to-json"), (err, tmpdir) => {
-      if (err) return reject(err);
+    fs.mkdtemp(
+      path.join(os.tmpdir(), "winget-to-json"),
+      async (err, tmpdir) => {
+        if (err) return reject(err);
 
-      http.get(url, async (response) => {
-        if (response.statusCode !== 200) {
+        const response = await fetch(url, {
+          headers: {
+            "Content-Type": "application/zip",
+          },
+        });
+        if (!response.ok) {
+          console.error(response);
           return reject(
             new Error(`Could not download winget manifests at ${url}`)
           );
@@ -19,7 +26,8 @@ export async function downloadWingetManifests(url: string): Promise<string> {
         const archiveFile = fs.createWriteStream(
           path.join(tmpdir, "archive.zip")
         );
-        response.pipe(archiveFile);
+        // For some reason, attempting to use a pipe here fails.
+        response.body.pipe(archiveFile);
 
         archiveFile.on("finish", async () => {
           archiveFile.close();
@@ -29,12 +37,12 @@ export async function downloadWingetManifests(url: string): Promise<string> {
 
           const extractedDir = path.join(tmpdir, "unarchived");
           fs.mkdirSync(extractedDir);
-          await archive.extract(".", extractedDir);
+          await archive.extract(null, extractedDir);
           await archive.close();
 
-          resolve(extractedDir);
+          resolve(path.join(extractedDir, fs.readdirSync(extractedDir)[0]));
         });
-      });
-    });
+      }
+    );
   });
 }
