1a51385da96d1e420b2a11f9eb73b3ad276c2bb4
Ian Gregory <justcheesy@outlook.com>
2022-09-26 20:18:50 -0400

    Remove unused utility types

--- src/renderer/util/Sanitized.ts
+++
@@ -1,7 +0,0 @@
-/**
- * The type of sanitized input.
- * 
- * Note: As users can obviously include `"__openstore_sanitized": true` in their input,
- * this should **not** be used as a runtime check.
- */
-type Sanitized<T> = T & { __openstore_sanitized: true };
