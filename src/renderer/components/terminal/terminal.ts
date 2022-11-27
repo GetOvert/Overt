6d2fd9bcb35c023bfa8fef4ad2dc18647cb06f02
Ian Gregory <justcheesy@outlook.com>
2022-11-27 15:15:27 -0500

    Way more performant log view thanks to WebGL renderer

--- src/renderer/components/terminal/terminal.ts
+++ src/renderer/components/terminal/terminal.ts
@@ -1,4 +1,5 @@
 import { Terminal } from "xterm";
+import { WebglAddon } from "xterm-addon-webgl";
 import "xterm/css/xterm.css";
 
 const xterm = new Terminal({
@@ -6,8 +7,17 @@ const xterm = new Terminal({
   cursorStyle: "bar",
   cols: 220,
   rows: 26,
+  // For some reason, the WebGL addon seems to require proposed API
+  // I can't find documentation stating this, but an error is thrown
+  // from `#loadAddon` if this is `false`
+  allowProposedApi: true,
 });
 
+const webglAddon = new WebglAddon();
+// https://github.com/xtermjs/xterm.js/tree/master/addons/xterm-addon-webgl#handling-context-loss
+webglAddon.onContextLoss(() => webglAddon.dispose());
+
 xterm.open(document.querySelector("#terminal")!);
+xterm.loadAddon(webglAddon);
 
 window.terminal.onReceive((data) => xterm.write(data));
