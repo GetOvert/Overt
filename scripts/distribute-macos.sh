9b6b2e6e6ff3c41e8af2be1e55de6a30e7d1bf3b
Ian Gregory <justcheesy@outlook.com>
2022-12-11 15:19:06 -0500

    Use `brew bump-cask-pr` to push updated cask defns

--- scripts/distribute-macos.sh
+++ scripts/distribute-macos.sh
@@ -23,22 +23,9 @@ version="$(git describe --tags --abbrev=0)"
 # Strip off 'v'
 version="${version:1}"
 
-arm64_sha256="$(openssl sha256 -binary "out/make/zip/darwin/arm64/Overt-darwin-arm64-$version.zip" | xxd -p -c 256)"
-x86_64_sha256="$(openssl sha256 -binary "out/make/zip/darwin/x64/Overt-darwin-x64-$version.zip" | xxd -p -c 256)"
-
-tap_repo="$(brew --repo homebrew/cask)"
-cask_definition_file="$tap_repo/Casks/overt.rb"
-
-# I'm sorry
-xcrun perl -pi -0 -e "s/(version\s*)\"[^\"]*\"/\$1\"$version\"/" "$cask_definition_file"
-xcrun perl -pi -0 -e "s/(?<=arm:   \")\w{64}/$arm64_sha256/" "$cask_definition_file"
-xcrun perl -pi -0 -e "s/(?<=intel: \")\w{64}/$x86_64_sha256/" "$cask_definition_file"
-
-git -C "$tap_repo" add "$cask_definition_file"
-git -C "$tap_repo" commit -m "Bump version to $version"
 cat <<END
-Once the binaries are on GitHub, manually push the updated cask definition with:
-  git -C "$tap_repo" push
+Once the binaries are on GitHub, update the cask definition with:
+  brew bump-cask-pr --version=$version overt
 END
 
 open "out/make/zip/darwin"
