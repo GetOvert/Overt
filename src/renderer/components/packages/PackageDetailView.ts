502645101fe4a5722394b46d8fae24dc4db22659
Ian Gregory <justcheesy@outlook.com>
2022-12-23 19:41:49 -0500

    Extract icons to dedicated shared file

--- src/renderer/components/packages/PackageDetailView.ts
+++ src/renderer/components/packages/PackageDetailView.ts
@@ -17,6 +17,7 @@ import {
 } from "./package-events";
 import { IPCPackageManager } from "ipc/package-managers/IPCPackageManager";
 import { html } from "lit";
+import { Icons } from "components/Icons";
 
 @customElement("openstore-package-detail-view")
 export default class PackageDetailView<PackageInfo> extends ProductView {
@@ -76,94 +77,6 @@ export default class PackageDetailView<PackageInfo> extends ProductView {
     return this.packageInfoAdapter.packageDetails(this.packageInfo);
   }
 
-  private static installIcon = html`
-    <svg
-      style="margin-bottom: 2px"
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      fill="currentColor"
-      class="bi bi-download"
-      viewBox="0 0 16 16"
-    >
-      <path
-        d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"
-      />
-      <path
-        d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"
-      />
-    </svg>
-  `;
-  private static updateIcon = html`
-    <svg
-      style="margin-bottom: 3px"
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      fill="currentColor"
-      class="bi bi-arrow-down"
-      viewBox="0 0 16 16"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
-      />
-    </svg>
-  `;
-  private static relaunchIcon = html`
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      fill="currentColor"
-      class="bi bi-arrow-clockwise"
-      viewBox="0 0 16 16"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"
-      />
-      <path
-        d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"
-      />
-    </svg>
-  `;
-  private static launchIcon = html`
-    <svg
-      style="transform: scale(0.9) translateY(-2px)"
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      fill="currentColor"
-      class="bi bi-box-arrow-up-right"
-      viewBox="0 0 16 16"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"
-      />
-      <path
-        fill-rule="evenodd"
-        d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"
-      />
-    </svg>
-  `;
-  private static uninstallIcon = html`
-    <svg
-      style="transform: scale(1.6) translateY(-0.75px)"
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      fill="currentColor"
-      class="bi bi-x"
-      viewBox="0 0 16 16"
-    >
-      <path
-        d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
-      />
-    </svg>
-  `;
-
   protected async buttons(): Promise<Button[]> {
     const packageIdentifier = this.packageInfoAdapter.packageIdentifier(
       this.packageInfo
@@ -176,7 +89,7 @@ export default class PackageDetailView<PackageInfo> extends ProductView {
     return [
       {
         title: "Install",
-        icon: PackageDetailView.installIcon,
+        icon: Icons.install({ style: "margin-bottom:2px" }),
         color: "primary",
 
         shown:
@@ -196,7 +109,7 @@ export default class PackageDetailView<PackageInfo> extends ProductView {
       },
       {
         title: "Relaunch",
-        icon: PackageDetailView.relaunchIcon,
+        icon: Icons.relaunch(),
         color: "success",
 
         shown: this.packageInfoAdapter.isPackageOvert(this.packageInfo),
@@ -211,7 +124,7 @@ export default class PackageDetailView<PackageInfo> extends ProductView {
         title: !additionalLaunchables.length
           ? "Launch"
           : `Launch ${mainLaunchable.label}`,
-        icon: PackageDetailView.launchIcon,
+        icon: Icons.launch(),
         color: "success",
 
         shown:
@@ -240,7 +153,7 @@ export default class PackageDetailView<PackageInfo> extends ProductView {
       },
       {
         title: "Update",
-        icon: PackageDetailView.updateIcon,
+        icon: Icons.upgrade({ style: "margin-bottom:3px" }),
         color: "primary",
 
         shown:
@@ -261,7 +174,9 @@ export default class PackageDetailView<PackageInfo> extends ProductView {
       },
       {
         title: "Uninstall",
-        icon: PackageDetailView.uninstallIcon,
+        icon: Icons.uninstall({
+          style: "transform: scale(1.6) translateY(-0.75px)",
+        }),
         color: "danger",
 
         shown: this.packageInfoAdapter.isPackageInstalled(this.packageInfo),
